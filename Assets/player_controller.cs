c1cc57d23e4f075bf6aab412d871095046817990
rkun <rkun123@hotmail.com>
2020-03-13 12:39:04 +0900

    水の表現と当たり判定とUIをとりあえず追加

--- Assets/player_controller.cs
+++ Assets/player_controller.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class player_controller : MonoBehaviour
 {
+    public int score;
+    float scoreMax = 1000.0f;
     public GameObject imageTarget;
+    public Image progressBar;
+    public Text scoreText;
     public float xRange = 10;
     private position_emitter positionEmitter;
     private float xLeftLimit = 0, xRightLimit = 0;
@@ -36,5 +41,15 @@ public class player_controller : MonoBehaviour
         Debug.Log("x:"+positionEmitter.transform.position.x);
         Debug.Log("Mug.x:"+x);
         return x;
+    }
+
+    public void onCollisionByWater() {
+        if(score < scoreMax) score++;
+        Debug.Log("Score: " + score);
+        RectTransform rectTransform = progressBar.GetComponent<RectTransform>();
+        rectTransform.localScale = new Vector3(1, score/scoreMax, 1);
+        scoreText.text = Mathf.Round(score / scoreMax * 100).ToString() + "%";
+
+
     }
 }
---
+++ Assets/water_collision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water_collision : MonoBehaviour
+{
+    public int numOfCollisions = 0;
+    public GameObject collisionTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject obj) {
+        if(obj.Equals(collisionTarget)) numOfCollisions++;
+        Debug.Log("Colisions: "+numOfCollisions);
+        player_controller player = obj.GetComponentInParent<player_controller>();
+        player.onCollisionByWater();
+
+    }
+}
