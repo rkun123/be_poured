1cce7f839360d4c3d6e7376d82dd92d4235ebed9
rkun <rkun123@hotmail.com>
2020-03-18 23:42:39 +0900

    ARマーカーを検知しているかどうか認識しインジケータを表示する機能を追加

---
+++ Assets/ARController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARController : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour trackableBehaviour;
+    public GameObject undetectedARMarkerIndicator;
+    private CanvasGroup undetectedARMarkerIndicatorCanvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trackableBehaviour = GetComponent<TrackableBehaviour>();
+        undetectedARMarkerIndicatorCanvasGroup = undetectedARMarkerIndicator.GetComponent<CanvasGroup>();
+
+        if(trackableBehaviour) trackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status prev, TrackableBehaviour.Status now) {
+        Debug.Log(now);
+        if(now == TrackableBehaviour.Status.DETECTED || now == TrackableBehaviour.Status.TRACKED || now == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+            // Don't show indicator
+            undetectedARMarkerIndicatorCanvasGroup.alpha = 0;
+        }else{
+            // Show indicator
+            undetectedARMarkerIndicatorCanvasGroup.alpha = 1;
+        }
+    }
+}
