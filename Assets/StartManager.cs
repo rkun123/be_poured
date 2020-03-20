518fb249cf9bff0e35818e4b0404b5a667b3079f
rkun <rkun123@hotmail.com>
2020-03-20 13:18:25 +0900

    スタートシーンを追加

---
+++ Assets/StartManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    SceneManager sceneManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneManager = new SceneManager();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Load Main scene");
+            SceneManager.LoadScene("Scenes/Main");
+        }
+    }
+}
